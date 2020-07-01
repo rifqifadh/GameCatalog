4f3c7c29ab884cc5ec2983327349fd5a136b8ff7
Rifqifadh <rifqi.fadhlillah@gmail.com>
2020-07-01 22:04:53 +0700

    Initial Commit

---
+++ Games Catalog/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Games Catalog
+//
+//  Created by Rifqi Fadh on 01/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Games Catalog/Games_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Games_CatalogApp.swift
+//  Games Catalog
+//
+//  Created by Rifqi Fadh on 01/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Games_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
